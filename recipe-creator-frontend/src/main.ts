7f8999a269f5579c8b2a4235fbc2eac628a3610e
Matthew D. Scholefield <matthew331199@gmail.com>
2026-09-10 14:05:40 +0800

    Add Eruda devtools on mobile
    (cherry picked from commit 528ca31a63f76f6553b7180667a783388cfd2ff1)

--- recipe-creator-frontend/src/main.ts
+++ recipe-creator-frontend/src/main.ts
@@ -1,4 +1,18 @@
 import { mount } from 'svelte';
 import App from './App.svelte';
 import './style.css';
+
+function isMobileDevice() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    || (navigator.maxTouchPoints > 1 && /Macintosh/i.test(navigator.userAgent));
+}
+
+async function initMobileDevtools() {
+  if (!isMobileDevice()) return;
+  // Keep the desktop bundle free of Eruda; it is only useful on mobile.
+  const { default: eruda } = await import('eruda');
+  eruda.init();
+}
+
+void initMobileDevtools();
 mount(App, { target: document.getElementById('app')! });
